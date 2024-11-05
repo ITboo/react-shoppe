8385b7a651f0fb03aea7978a49a61c6c89954098
ITboo <annaacdc@tut.by>
2024-11-05 21:22:43 +0300

    chore: update validation, add error-messages, add reset form after submision

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,22 +6,21 @@ type ContactForm = {
   last_name: string;
   email: string;
   message: string;
+  phone: string;
 };
 const Contact = () => {
   const {
     register,
     handleSubmit,
     reset,
-    formState: { errors },
-  } = useForm<ContactForm>();
+    formState: { errors, isValid },
+  } = useForm<ContactForm>({ mode: "onBlur" });
   const submit: SubmitHandler<ContactForm> = (data) => {
     console.log(data);
+    reset();
   };
   const error: SubmitErrorHandler<ContactForm> = (data) => {
     console.log(data);
-  };
-  const isEmail = (_) => {
-    return true;
   };
 
   return (
@@ -36,23 +35,75 @@ const Contact = () => {
 
       <form className="contact-form" onSubmit={handleSubmit(submit, error)}>
         <div className="form-wrapper">
-          <input
-            type="text"
-            placeholder="First Name"
-            aria-invalid={errors.first_name ? true : false}
-            {...register("first_name", { required: true, validate: isEmail })}
-          />
-          <input
-            type="text"
-            placeholder="Last Name"
-            {...register("last_name", { required: true, validate: isEmail })}
-          />
-          <input
-            type="email"
-            placeholder="email"
-            {...register("email", { required: true, validate: isEmail })}
-          />
-          <input type="tel" placeholder="phone" />
+          <div className="">
+            <input
+              type="text"
+              placeholder="First Name"
+              aria-invalid={errors.first_name ? true : false}
+              {...register("first_name", {
+                required: "This field is required ",
+                minLength: {
+                  value: 5,
+                  message: "Minimum 5 symbols",
+                },
+              })}
+            />
+            <div className="err">
+              {errors?.first_name && (
+                <p>
+                  {errors?.first_name?.message || "Oops, something went wrong!"}
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="">
+            <input
+              type="text"
+              placeholder="Last Name"
+              {...register("last_name", {
+                required: "This field is required ",
+                minLength: {
+                  value: 5,
+                  message: "Minimum 5 symbols",
+                },
+              })}
+            />
+            <div className="err">
+              {errors?.last_name && (
+                <p>
+                  {errors?.last_name?.message || "Oops, something went wrong!"}
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="">
+            <input
+              type="email"
+              placeholder="Email"
+              {...register("email", {
+                required: "This field is required ",
+              })}
+            />
+            <div className="err">
+              {errors?.email && (
+                <p>{errors?.email?.message || "Oops, something went wrong!"}</p>
+              )}
+            </div>
+          </div>
+          <div className="">
+            <input
+              type="tel"
+              placeholder="Phone"
+              {...register("phone", {
+                required: "This field is required ",
+              })}
+            />
+            <div className="err">
+              {errors?.phone && (
+                <p>{errors?.phone?.message || "Oops, something went wrong!"}</p>
+              )}
+            </div>
+          </div>
         </div>
         <textarea
           className="contact-textarea"
@@ -61,10 +112,12 @@ const Contact = () => {
           cols="30"
           rows="10"
           placeholder="Message"
-          {...register("message", { required: true, validate: isEmail })}
+          {...register("message", { required: true })}
         ></textarea>
         <div className="contact-btns">
-          <button className="send">Send</button>
+          <button className={isValid ? "send" : "disabled"} disabled={!isValid}>
+            Send
+          </button>
           <button type="button" onClick={() => reset()} className="reset">
             Reset
           </button>
