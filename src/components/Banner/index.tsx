653cdb36cee63212a4c339ddcfd6713ccd0b50fa
ITboo <annaacdc@tut.by>
2024-11-07 22:01:35 +0300

    feat: add countdown timer

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,7 +1,15 @@
-import bannerImg from "../../assets/img/banner.jpg";
-
+import { Link } from "react-router-dom";
+import './Banner.css'
 const Banner = () => {
-  return <img src={bannerImg} alt="banner" />;
+  return (
+    <div className="banner">
+      <div className="banner__info">
+      <h2 className="banner__title">Gold big hoops </h2>
+      <h3 className="banner__subtitle">$ 68,00</h3>
+      <Link to='/' className="banner__btn">View Product</Link>
+    </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,8 +1,67 @@
+import { useEffect, useState } from "react";
+import "./style/Shop.css";
+import CardList from "../components/CardList";
 const Shop = () => {
+  const [expiryTime, setExpiryTime] = useState("31 dec 2024 00:00:00");
+  const [countdownTime, setCountdownTime] = useState({
+    countdownDays: "",
+    countdownHours: "",
+    countdownMinutes: "",
+    countdownSeconds: "",
+  });
+
+  const countdownTimer = () => {
+    const timeInterval = setInterval(() => {
+      const countdownDateTime = new Date(expiryTime).getTime();
+      const currentTime = new Date().getTime();
+      const remainingDayTime = countdownDateTime - currentTime;
+      const totalDays = Math.floor(remainingDayTime / (1000 * 60 * 60 * 24));
+      const totalHours = Math.floor(
+        (remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const totalMinutes = Math.floor(
+        (remainingDayTime % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const totalSeconds = Math.floor((remainingDayTime % (1000 * 60)) / 1000);
+
+      const runningCountdownTime = {
+        countdownDays: totalDays,
+        countdownHours: totalHours,
+        countdownMinutes: totalMinutes,
+        countdownSeconds: totalSeconds,
+      };
+
+      setCountdownTime(runningCountdownTime);
+
+      if (remainingDayTime < 0) {
+        clearInterval(timeInterval);
+        setExpiryTime(false);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    countdownTimer();
+  }, []);
+
   return (
-    <>
-      shop
-    </>
+    <section>
+      <div className="christmas">
+        <h1>Great Christmas Sale!</h1>
+        <p>Choose the best gifts for your beloved</p>
+        <div className="countdown">
+          <div className="countdown__item"><span>DAYS</span><span>{countdownTime.countdownDays}</span></div>
+          <div className="countdown__item"><span>HOURS</span><span>{countdownTime.countdownHours}</span></div>
+          <div className="countdown__item">
+            <span>MINUTES</span><span>{countdownTime.countdownMinutes}</span>
+          </div>
+          <div className="countdown__item">
+            <span>SECONDS</span><span>{countdownTime.countdownSeconds}</span>
+          </div>
+        </div>
+      </div>
+      <CardList/>
+    </section>
   );
 };
 
